4d58f190a4f0be91ae0ea9172557dbafbde768ee
MARK PEARYER <markpearyer@MARKs-Air.attlocal.net>
2015-06-01 20:56:57 -0700

    Refactored the code again to be more modular.

--- refactorChallenge.js
+++ refactorChallenge.js
@@ -1,16 +1,39 @@
 var html = '';
-var red;
-var green;
-var blue;
+//var red;
+//var green;
+//var blue;
 var rgbColor;
-for (var i = 0, i =< 4; i++) {
-  red = Math.floor(Math.random() * 256 );
-  green = Math.floor(Math.random() * 256 );
-  blue = Math.floor(Math.random() * 256 );
-  rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-  html += '<div style="background-color:' + rgbColor + '"></div>';
-  document.write(html);
+
+
+//adding a random function that will select a random color 
+function ranColor() {
+  return Math.floor(Math.random() * 256);
 }
+
+
+//creating a random rgb value 
+function ranRGB() {
+  var color = 'rgb(';
+  color += ranColor() + ',';
+  color += ranColor() + ',';
+  color += ranColor() + ',';
+  return color;
+}
+
+
+//Print
+function print(message) {
+  document.write(message);
+}
+
+//Print the circles
+for (var i = 0, i =< 4; i++) {
+  rgbColor = ranRGB();
+  html += '<div style="background-color:' + rgbColor + '"></div>';
+  
+}
+print(html);
+
 
 /* red = Math.floor(Math.random() * 256 );
 green = Math.floor(Math.random() * 256 );
